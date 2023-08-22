889610f57e1fd49af7a41973e7e347a20f3ec5ff
Abhijay K <abhijay320@gmail.com>
2023-08-23 01:29:10 +0530

    Updated sql seed data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
-DELETE FROM USER WHERE email IN ("admin@admin.com", "clarkkent@krypton.com");
-INSERT INTO USER (email, password, role)VALUES("admin@admin.com", "$2a$10$bPmKC5MhhxqVYfyKuHeD8OX0eSnSyEyleXmrVNs9CoBvPSW9JQqs2", 1);
-INSERT INTO USER (email, password, role)VALUES("clarkkent@krypton.com", "$2a$10$.EWi5440kUJbwe6L3VpiSuIqr9kO4qIaTLMcpKce2OdFeVsKSEqXy", 0);
+INSERT INTO USER (id,email, password, role)
+SELECT 1, "admin@admin.com", "$2a$10$bPmKC5MhhxqVYfyKuHeD8OX0eSnSyEyleXmrVNs9CoBvPSW9JQqs2", 1 FROM DUAL WHERE
+NOT EXISTS (SELECT 1 FROM user where email = "admin@admin.com");
+
+INSERT INTO USER (id,email, password, role)
+SELECT 2, "clarkkent@krypton.com", "$2a$10$.EWi5440kUJbwe6L3VpiSuIqr9kO4qIaTLMcpKce2OdFeVsKSEqXy", 0 FROM DUAL WHERE
+NOT EXISTS (SELECT 1 FROM user where email = "clarkkent@krypton.com");
+
